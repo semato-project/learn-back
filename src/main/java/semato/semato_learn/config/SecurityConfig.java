583e72659a7246fc7c9c66cf1c14fea453c6a6cd
MrWonsik <mrt.wonsik@gmail.com>
2020-03-30 20:07:10 +0200

    install swagger

--- src/main/java/semato/semato_learn/config/SecurityConfig.java
+++ src/main/java/semato/semato_learn/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import semato.semato_learn.util.security.CustomUserDetailsService;
 import semato.semato_learn.util.security.JwtAuthenticationEntryPoint;
 import semato.semato_learn.util.security.JwtAuthenticationFilter;
@@ -81,7 +82,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                 .permitAll()
-                .antMatchers("/api/auth/**")
+                .antMatchers("/api/auth/**",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/v2/api-docs",
+                        "/webjars/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
