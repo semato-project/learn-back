7e67b695b47786aecc5d2b3bd2586530b0b1ee7c
Sebastian <sebastian.tatarczuch@gmail.com>
2020-05-03 20:33:47 +0200

    restfulness

--- src/main/java/semato/semato_learn/controller/CourseController.java
+++ src/main/java/semato/semato_learn/controller/CourseController.java
@@ -7,10 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import semato.semato_learn.controller.payload.CourseRequest;
+import semato.semato_learn.controller.payload.ProjectGroupResponse;
+import semato.semato_learn.model.ProjectGroup;
+import semato.semato_learn.model.Student;
 import semato.semato_learn.service.CourseService;
+import semato.semato_learn.service.ProjectGroupService;
 import semato.semato_learn.util.security.CurrentUser;
 import semato.semato_learn.util.security.UserPrincipal;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.LinkedList;
 
 @RestController
 @RequestMapping("/api/course")
@@ -18,6 +24,9 @@ public class CourseController {
 
     @Autowired
     private CourseService courseService;
+
+    @Autowired
+    private ProjectGroupService projectGroupService;
 
     @Secured({"ROLE_LECTURER"})
     @PostMapping("/")
@@ -44,6 +53,33 @@ public class CourseController {
     @Secured({"ROLE_LECTURER", "ROLE_STUDENT"})
     public ResponseEntity getExtended(@PathVariable("id") Long courseId, @ApiIgnore @CurrentUser UserPrincipal currentUser){
         return ResponseEntity.ok(courseService.getExtended(courseId, currentUser.getUser()));
+    }
+
+    @PostMapping("/{id}/project-group")
+    @Secured({"ROLE_STUDENT"})
+    @ApiOperation(value = "Creates ProjectGroup for Course")
+    public ResponseEntity create(@PathVariable("id") Long id, @ApiIgnore @CurrentUser UserPrincipal currentUser) {
+        try {
+            projectGroupService.create((Student) currentUser.getUser(), id);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/project-group")
+    @Secured({"ROLE_STUDENT"})
+    @ApiOperation(value = "Returns existing projectGroup list for course.")
+    public ResponseEntity getByCourse(@PathVariable("id") Long id, @ApiIgnore @CurrentUser UserPrincipal currentUser) {
+        try {
+            LinkedList<ProjectGroupResponse> projectGroupResponseList = new LinkedList<ProjectGroupResponse>();
+            for (ProjectGroup projectGroup: projectGroupService.getProjectGroupListForCourse(id)) {
+                projectGroupResponseList.add(new ProjectGroupResponse(projectGroup));
+            }
+            return ResponseEntity.ok(projectGroupResponseList);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
--- src/main/java/semato/semato_learn/controller/ProjectGroupController.java
+++ src/main/java/semato/semato_learn/controller/ProjectGroupController.java
@@ -5,36 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
-import semato.semato_learn.controller.payload.ProjectGroupResponse;
-import semato.semato_learn.model.ProjectGroup;
 import semato.semato_learn.model.Student;
 import semato.semato_learn.service.ProjectGroupService;
 import semato.semato_learn.util.security.CurrentUser;
 import semato.semato_learn.util.security.UserPrincipal;
 import springfox.documentation.annotations.ApiIgnore;
 
-import java.util.LinkedList;
-
 @RestController
-@RequestMapping("/api/project_group")
+@RequestMapping("/api/project-group")
 public class ProjectGroupController {
 
     @Autowired
     private ProjectGroupService projectGroupService;
 
-    @PostMapping("/for_course/{id}")
-    @Secured({"ROLE_STUDENT"})
-    @ApiOperation(value = "Creates ProjectGroup for Course")
-    public ResponseEntity create(@PathVariable("id") Long id, @ApiIgnore @CurrentUser UserPrincipal currentUser) {
-        try {
-            projectGroupService.create((Student) currentUser.getUser(), id);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("/join/{id}")
+    @PutMapping("/{id}")
     @Secured({"ROLE_STUDENT"})
     @ApiOperation(value = "Join ProjectGroup for Task")
     public ResponseEntity join(@PathVariable("id") Long id, @ApiIgnore @CurrentUser UserPrincipal currentUser) {
@@ -44,21 +28,6 @@ public class ProjectGroupController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
         return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/for_course/{id}")
-    @Secured({"ROLE_STUDENT"})
-    @ApiOperation(value = "Returns existing projectGroup list for course.")
-    public ResponseEntity getByCourse(@PathVariable("id") Long id, @ApiIgnore @CurrentUser UserPrincipal currentUser) {
-        try {
-            LinkedList<ProjectGroupResponse> projectGroupResponseList = new LinkedList<ProjectGroupResponse>();
-            for (ProjectGroup projectGroup: projectGroupService.getProjectGroupListForCourse(id)) {
-                projectGroupResponseList.add(new ProjectGroupResponse(projectGroup));
-            }
-            return ResponseEntity.ok(projectGroupResponseList);
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
     }
 
 }
